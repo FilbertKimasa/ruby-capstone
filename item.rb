b50246fab62dc81671edd327a3217f28479f28ba
FilbertKimasa <filbertdan67@gmail.com>
2023-12-12 22:33:16 +0300

    Set archived and can_be_archived private

--- item.rb
+++ item.rb
@@ -3,13 +3,10 @@
 class Item
   attr_writer :genre, :author, :source, :label
 
-  def initialize(published_date, archived: false)
+  def initialize(published_date)
+    @id = Random.rand(1...1000)
     @published_date = published_date
-    @archived = archived
-  end
-
-  def can_be_archived?
-    (Time.now.year - @published_date.year) > 10
+    @archived = false
   end
 
   def move_to_archive
@@ -19,5 +16,13 @@ class Item
     else
       puts "This item cannot be archived at this time."
     end
+  end
+
+  private
+
+  attr_accessor :archived
+
+  def can_be_archived?
+    (Time.now.year - @published_date.year) > 10
   end
 end
