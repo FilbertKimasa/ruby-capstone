c30cb5445979fb37692fc4243b9a31dfc0ac3796
FilbertKimasa <filbertdan67@gmail.com>
2023-12-12 23:16:33 +0300

    Implement Genre class

---
+++ genre.rb
@@ -0,0 +1,21 @@
+# genre.rb
+
+class Genre
+  attr_reader :name
+
+  def initialize(name)
+    @id = Random.rand(1...1000)
+    @name = name
+    @items = []
+  end
+
+  def add_item(item)
+    @items << item
+  end
+
+  private
+
+  attr_reader :id
+  attr_accessor :items
+
+end
