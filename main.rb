84918bea3216f3c8a4bbf3c5e79a49c89567e892
Noel Bryant <noelsobryant@gmail.com>
2023-12-13 14:01:14 +0300

    create app entry point

---
+++ main.rb
@@ -0,0 +1,40 @@
+require_relative 'app'
+require_relative 'menu'
+
+# app entry point
+class Catalog
+  def initialize
+    @app = App.new
+    @menu = Menu.new(@app)
+  end
+
+  def start
+
+    display_welcome_message
+
+    loop do
+      @menu.display
+      option = user_input
+      break if @menu.handle_option(option) == :exit
+    end
+
+    exit_program
+  end
+
+  private
+
+  def display_welcome_message
+    puts 'Welcome to my catalog!'
+    puts ' '
+  end
+
+  def user_input
+    gets.chomp.to_i
+  end
+
+  def exit_program
+    puts 'Thank you for using this app! Goodbye!'
+  end
+end
+
+Catalog.new.start
