2343eb2578065a8da7ddaef628f429cff47c5a1c
Noel Bryant <noelsobryant@gmail.com>
2023-12-13 21:32:24 +0300

    create setter methods for one to many relationships

--- item.rb
+++ item.rb
@@ -1,7 +1,7 @@
 # item.rb
-
 class Item
-  attr_accessor :genre, :author, :source, :label
+  attr_accessor :published_date
+  attr_reader :genre, :author, :source, :label
 
   def initialize(published_date)
     @id = Random.rand(1...1000)
@@ -18,6 +18,26 @@ class Item
     end
   end
 
+  def genre=(genre)
+    @genre = genre
+    genre.items.push(self) unless genre.items.include?(self)
+  end
+
+  def author=(author)
+    @author = author
+    author.items.push(self) unless author.items.include?(self)
+  end
+
+  def source=(source)
+    @source = source
+    source.items.push(self) unless source.items.include?(self)
+  end
+
+  def label=(label)
+    @label = label
+    label.items.push(self) unless label.items.include?(self)
+  end
+  
   private
 
   attr_accessor :archived
