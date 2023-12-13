ccba390b2d950e54e348bdd784278403c51b530e
Noel Bryant <noelsobryant@gmail.com>
2023-12-13 13:53:01 +0300

    create app class to handle core functionality

---
+++ app.rb
@@ -0,0 +1,25 @@
+require_relative 'item'
+
+# Core functionality class
+class App
+  attr_accessor :item
+
+  def initialize
+    @item = []
+  end
+
+  def create_item
+    print 'Enter Genre: '
+    genre = gets.chomp
+    print 'Enter Author: '
+    author = gets.chomp
+    print 'Enter source: '
+    source = gets.chomp
+    print 'Enter label: '
+    label= gets.chomp
+    item = Item.new(genre)
+    @item << item
+    puts 'Item Created Successfully'
+    puts ' '
+  end
+end
