efc9b4c6c8706ba781ab3b4a9ade2717f48f7735
Noel Bryant <noelsobryant@gmail.com>
2023-12-15 02:03:31 +0300

    handle append data to json file

--- file_operations.rb
+++ file_operations.rb
@@ -21,18 +21,21 @@ class FileOperations
   end
 
   def save_to_file(file_name, data)
+    existing_data = load_from_file(file_name) || [] # Load existing data or initialize with an empty array
+    updated_data = existing_data + data.map do |book|
+      {
+        'id' => book.id,
+        'publish_date' => book.published_date || '', # Handle nil case
+        'publisher' => book.publisher,
+        'cover_state' => book.cover_state
+      }
+    end
+
     File.open(file_name, 'w') do |file|
-      data = data.map do |book|
-        {
-          'id' => book.id,
-          'publish_date' => book.published_date || '', # Handle nil case
-          'publisher' => book.publisher,
-          'cover_state' => book.cover_state
-        }
-      end
-      file.write(JSON.generate(data))
+      file.puts(JSON.generate(updated_data))
     end
   end
+
 
   def load_from_file(file_name, &block)
     return unless File.exist?(file_name)
