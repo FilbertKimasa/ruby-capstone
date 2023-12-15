ba1834a485b8d32dee3c481a8461beae3e9ddd23
Noel Bryant <noelsobryant@gmail.com>
2023-12-15 21:49:45 +0300

    add published date attribute

--- game.rb
+++ game.rb
@@ -2,13 +2,14 @@ require_relative 'item'
 require 'date'
 
 class Game < Item
-  attr_accessor :multiplayer, :last_played_at
+  attr_accessor :multiplayer, :last_played_at, :published_date
   attr_reader :id
 
   def initialize(published_date, multiplayer, last_played_at)
     super(published_date)
     @multiplayer = multiplayer
     @last_played_at = Date.parse(last_played_at.to_s)
+    @published_date = published_date
   end
 
   def can_be_archived?
