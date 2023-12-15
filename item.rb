3fb1fdf7436677b1149a690dadf45fd8313cebc8
Noel Bryant <noelsobryant@gmail.com>
2023-12-15 21:36:19 +0300

    fix published date

--- item.rb
+++ item.rb
@@ -5,7 +5,7 @@ class Item
 
   def initialize(published_date)
     @id = Random.rand(1...1000)
-    @published_date = published_date
+    @published_date = Date.parse(published_date.to_s)  # Ensure it's a Date object
     @archived = false
   end
 
